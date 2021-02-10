6ffd104d0089cc0e0e31729d6394ed0644065554
rlund760 <robdlundberg@gmail.com>
2021-02-10 10:28:45 -0800

    added length values + alert

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  var passwordLength = parseInt(prompt("Enter Password Length"))
+  if (passwordLength < 8 || passwordLength>121){
+   alert ("Enter valid length between 8 and 21") 
+   return "Enter valid length between 8 and 21"
+  }
+}
